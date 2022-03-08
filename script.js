c72be99d892d353f8e68b6c807f3e037801cabc8
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2022-03-08 11:10:42 -0500

    Add a loop to play five games and declare a winner

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 let computerSelection;
 let userSelection;
+let computerScore=0;
+let userScore=0;
 
 function computerPlay(){
     computerPick= Math.floor(Math.random() * 3);
     (computerPick===0)?computerSelection="rock":
         (computerPick===1)?computerSelection="paper":computerSelection="scissors";
-    console.log(computerSelection);
+    console.log(`Computer chose ${computerSelection}`);
     return computerSelection;
 }
 
 function userPlay(){
     userSelection=(prompt("Choose Rock, Paper or Scissors!")).toLowerCase();  //userSelection is case insensitive
-    console.log(userSelection);
+    console.log(`Player chose ${userSelection}`);
     return userSelection;
 }
 
 function gameRound(computerPlay,userPlay) {
-    computerPlay();
     userPlay();
+    computerPlay();
     if (computerSelection===userSelection){
         console.log("The game ended in a draw:no winner")
     }
 
     else if ((computerSelection==="rock" && userSelection==="scissors")  || (computerSelection==="paper" && userSelection==="rock") || (computerSelection==="scissors" && userSelection==="paper")) {
-        console.log(`Computer won: ${computerSelection} beats ${userSelection}!`)
+        console.log(`Computer won: ${computerSelection} beats ${userSelection}!`);
+        computerScore+=1;
     }
 
     else {
-        console.log(`Player won: ${userSelection} beats ${computerSelection}!`)
+        console.log(`Player won: ${userSelection} beats ${computerSelection}!`);
+        userScore+=1;
     }
 }
 
-gameRound(computerPlay,userPlay);
+function game(){
+    for (let i=1; i<=5 ; i++) {
+        console.log(`game ${i}`)
+        gameRound(computerPlay,userPlay);
+    }
+    (computerScore===userScore)? console.log("The game ended in a draw:no winner"):
+        (computerScore>userScore)? console.log("Computer wins the GAME!"):console.log("Player wins the GAME!")
+}
+
+game();
