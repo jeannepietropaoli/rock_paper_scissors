0de4854c8140ced03edb64134d148ccc7aa29218
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2022-03-21 15:06:50 -0400

    Add basic UI(without style)

--- script.js
+++ script.js
@@ -2,48 +2,71 @@ let computerSelection;
 let userSelection;
 let computerScore=0;
 let userScore=0;
+const playButtons = document.querySelectorAll('button');
+const results = document.querySelector('.results');
+
+let userResult = document.createElement('p');
+results.appendChild(userResult);   //crée le paragraphe des resultats de User
+
+let computerResult = document.createElement('p');
+results.appendChild(computerResult);   //crée le paragraphe des resultats de Computer
+
+let miniGameResult = document.createElement('p');
+results.appendChild(miniGameResult);     //crée le paragraphe des resultats de chaque MiniGame
+
+let gameResult = document.createElement('p');
+results.appendChild(gameResult); 
+
+let score = document.querySelector('.score');
+displayScore()
+
+function displayScore() {
+    score.textContent = `Player = ${userScore} - Computer ${computerScore}`;
+}
 
 function computerPlay(){
     computerPick= Math.floor(Math.random() * 3);
     (computerPick===0)?computerSelection="rock":
         (computerPick===1)?computerSelection="paper":computerSelection="scissors";
-    console.log(`Computer chose ${computerSelection}`);
+    computerResult.textContent =`Computer chose ${computerSelection}`;
     return computerSelection;
 }
 
 function userPlay(){
-    userSelection=(prompt("Choose Rock, Paper or Scissors!")).toLowerCase();  //userSelection is case insensitive
-    console.log(`Player chose ${userSelection}`);
-    return userSelection;
+    userResult.textContent = (`Player chose ${userSelection}`);
+        return userSelection;
 }
 
 function gameRound(computerPlay,userPlay) {
-    userPlay();
+    userPlay()
     computerPlay();
-    if (computerSelection===userSelection){
-        console.log("The game ended in a draw:no winner")
-    }
 
+    if (computerSelection===userSelection){
+        miniGameResult.textContent = "The game ended in a draw:no winner";
+    }
     else if ((computerSelection==="rock" && userSelection==="scissors")  ||
                  (computerSelection==="paper" && userSelection==="rock") || 
                     (computerSelection==="scissors" && userSelection==="paper")) {
-        console.log(`Computer won: ${computerSelection} beats ${userSelection}!`);
-        computerScore+=1;
+        miniGameResult.textContent = `Computer won: ${computerSelection} beats ${userSelection}!`;
+        computerScore++;
     }
-
     else {
-        console.log(`Player won: ${userSelection} beats ${computerSelection}!`);
-        userScore+=1;
+        miniGameResult.textContent = `Player won: ${userSelection} beats ${computerSelection}!`;
+        userScore++;
     }
+    displayScore();
 }
 
-function game(){
-    for (let i=1; i<=5 ; i++) {
-        console.log(`game ${i}`)
+playButtons.forEach((playButton) => { 
+    playButton.addEventListener('click', function game(){
+        userSelection=this.textContent.toLowerCase();
         gameRound(computerPlay,userPlay);
-    }
-    (computerScore===userScore)? console.log("The game ended in a draw:no winner"):
-        (computerScore>userScore)? console.log("Computer wins the GAME!"):console.log("Player wins the GAME!")
-}
+        gameResult.textContent = '';
 
-game();
+        if (computerScore===5 || userScore===5){
+            (computerScore===userScore)? gameResult.textContent = "The game ended in a draw:no winner":
+            (computerScore>userScore)? gameResult.textContent ="Computer wins the GAME!" : gameResult.textContent ="Player wins the GAME!";
+            userScore=0;
+            computerScore=0;
+        }
+    })})
