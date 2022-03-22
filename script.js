77255214febb1e5facad08d4b9ce0a0cf57a2526
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2022-03-22 16:58:50 -0400

    Add highlights to the sign's winner
    and info for the user about it

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let userSelection;
 let computerScore=0;
 let userScore=0;
 let isGameRunning=true;
+let nbMiniGame = 0;
 const playButtons = document.querySelectorAll('.button');
 const results = document.querySelector('.results');
 const signScorePlayer = document.querySelector('.signScorePlayer');
@@ -13,6 +14,8 @@ const signScoreLogoPlayer = document.querySelector('.signScoreLogoPlayer');
 const signScoreLogoComputer = document.querySelector('.signScoreLogoComputer');
 gameReslut.textContent='Who will win the game?';
 let winner;
+const compteur = document.createElement('div');
+document.body.appendChild(compteur);
 
 displayScore();
 higlightWinnerLogo();
@@ -34,25 +37,29 @@ function computerPlay(){
 
 function userPlay(){
     signScoreLogoPlayer.setAttribute('src',`pic/${userSelection}_neon_black.png`);
-        return userSelection;
+    return userSelection;
 }
 
-function higlightWinnerLogo(winner){
-signScoreLogoComputer.addEventListener('mouseover', function higlightC(){
-    if (winner==='computer'){
-    signScoreLogoComputer.setAttribute('src',`pic/${computerSelection}_neon_black3.png`);
-    signScoreLogoComputer.addEventListener('mouseout', function (){
-        signScoreLogoComputer.setAttribute('src',`pic/${computerSelection}_neon_black.png`);})
-    }
+function higlightWinnerLogo(){
+    signScoreLogoComputer.addEventListener('mouseover', function higlightC(){
+        if (winner==='computer'){
+            signScoreLogoComputer.setAttribute('src',`pic/${computerSelection}_neon_black3.png`);
+            signScoreLogoComputer.addEventListener('mouseout', function (){
+                signScoreLogoComputer.setAttribute('src',`pic/${computerSelection}_neon_black.png`);
+            })
+        }
+    })
 
     signScoreLogoPlayer.addEventListener('mouseover', function higlightP(){
         if (winner==='player'){
-        signScoreLogoPlayer.setAttribute('src',`pic/${userSelection}_neon_black3.png`);
-        signScoreLogoPlayer.addEventListener('mouseout', function (){
-            signScoreLogoPlayer.setAttribute('src',`pic/${userSelection}_neon_black.png`);})
-        } })
-
-
+            signScoreLogoPlayer.setAttribute('src',`pic/${userSelection}_neon_black3.png`);
+            signScoreLogoPlayer.addEventListener('mouseout', function (){
+                signScoreLogoPlayer.setAttribute('src',`pic/${userSelection}_neon_black.png`);
+            })
+        }
+    })
+}
+    
 function gameRound(computerPlay,userPlay) {
     winner='';
     userPlay()
@@ -75,22 +82,54 @@ function gameRound(computerPlay,userPlay) {
     displayScore();
 }
 
-playButtons.forEach((playButton) => { 
+function playAgain(){
+    const playAgainButton = document.createElement('button');
+    document.querySelector('.playAgain').appendChild(playAgainButton);
+    playAgainButton.textContent = "Another round? Just click!";
+    playAgainButton.addEventListener('click', function() {
+        isGameRunning=true;
+        userScore=0;
+        computerScore=0;
+        displayScore();
+        signScoreLogoPlayer.setAttribute('src', 'pic/black.jpg');
+        signScoreLogoComputer.setAttribute('src', 'pic/black.jpg');
+        miniGameResult.textContent = '';
+        gameReslut.textContent = '';
+        playAgainButton.remove();
+        gameReslut.textContent='Who will win the game?';
+        nbMiniGame=0;
+    }) 
+    
+}
+
+function displayHoverInfo () {
+    nbMiniGame++;
+    console.log(nbMiniGame);
+    if (nbMiniGame===1){
+        compteur.textContent = 'Fun fact: hover over the scoreboard signs to see the winner\'s sign highlighted';
+    }
+    else if (nbMiniGame>1){
+        compteur.textContent='';
+    }
+}
+
+playButtons.forEach ((playButton) => { 
     playButton.addEventListener('mouseover', function (){
         let start = playButton.src;
         let hover = playButton.getAttribute('alt'); //specified in img tag.
         playButton.src = hover; 
         playButton.addEventListener('mouseout', function (){
-        playButton.src = start;
+            playButton.src = start;
+        })
     })
-    })
-
     
     playButton.addEventListener('click', function game(){
         userSelection=this.getAttribute('id');
+        displayHoverInfo();
         if (isGameRunning){
             gameRound(computerPlay,userPlay);
             higlightWinnerLogo(winner);
+            
         
         if (computerScore===5 || userScore===5){
             if (computerScore===userScore){
@@ -100,27 +139,11 @@ playButtons.forEach((playButton) => {
             } else { 
                 gameReslut.textContent = "Player wins the GAME!";
             }
-
-            isGameRunning=false;
-
-            function playAgain(){
-                const playAgainButton = document.createElement('button');
-                document.querySelector('.playAgain').appendChild(playAgainButton);
-                playAgainButton.textContent = "Another round? Just click!";
-                playAgainButton.addEventListener('click', function() {
-                    isGameRunning=true;
-                    userScore=0;
-                    computerScore=0;
-                    displayScore();
-                    signScoreLogoPlayer.setAttribute('src', 'pic/black.jpg');
-                    signScoreLogoComputer.setAttribute('src', 'pic/black.jpg');
-                    miniGameResult.textContent = '';
-                    gameReslut.textContent = '';
-                    playAgainButton.remove();
-                    gameReslut.textContent='Who will win the game?';
-                })
-            }
             playAgain();
+            isGameRunning=false;
         }
-    } }
-    
+    } 
+    })
+})
+       
+
